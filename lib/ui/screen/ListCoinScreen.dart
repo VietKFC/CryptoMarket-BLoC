e1ac413516c9aafbc8810978b4c618f448438eb5
Nguyễn Hữu Nam <nguyen.huu.nam-b@sun-asterisk.com>
2022-01-06 15:45:23 +0700

    handle search listcoin screen

--- lib/ui/screen/ListCoinScreen.dart
+++ lib/ui/screen/ListCoinScreen.dart
@@ -6,6 +6,7 @@ import 'package:vn_crypto/bloc/list_coin/list_coin_state.dart';
 import 'package:vn_crypto/data/model/item_coin.dart';
 import 'package:vn_crypto/data/repository/coins_repository.dart';
 import 'package:vn_crypto/di/dependency_injection.dart';
+import 'package:vn_crypto/ui/components/common/CoinSearchBar.dart';
 import 'package:vn_crypto/ui/components/items/ListCoinItem.dart';
 import 'package:vn_crypto/ultils/Constraint.dart';
 
@@ -23,31 +24,41 @@ class ListCoinScreen extends StatelessWidget {
           onRefresh: () async {
             listCoinBloc.add(ListCoinLoaded());
           },
-          child: Scaffold(
-            appBar: AppBar(
-                title: const Text(AppStrings.titleListTopCoin,
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(color: Colors.black)),
-                actions: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.search),
-                    color: Colors.black,
-                  )
-                ]),
-            body: BlocBuilder<ListCoinBloc, ListCoinState>(
-              builder: (context, state) {
-                if (state is ListCoinLoadSuccess) {
-                  return listCoin(list: state.coins);
-                } else if (state is ListCoinLoading) {
-                  return const Center(child: CircularProgressIndicator());
-                } else {
-                  return const Center(child: Text(AppStrings.textEmptyList));
-                }
-              },
-            ),
+          child: BlocBuilder<ListCoinBloc, ListCoinState>(
+            builder: (context, state) {
+              return Scaffold(
+                  appBar: AppBar(
+                      title: const Text(AppStrings.titleListTopCoin,
+                          textDirection: TextDirection.ltr,
+                          style: TextStyle(color: Colors.black)),
+                      actions: [
+                        IconButton(
+                          onPressed: () {
+                            showSearch(
+                                context: context,
+                                delegate: CoinSearch(
+                                    listCoin: state is ListCoinLoadSuccess
+                                        ? state.coins
+                                        : []));
+                          },
+                          icon: const Icon(Icons.search),
+                          color: Colors.black,
+                        )
+                      ]),
+                  body: buildBody(state: state));
+            },
           ),
         ));
+  }
+
+  Widget buildBody({var state}) {
+    if (state is ListCoinLoadSuccess) {
+      return listCoin(list: state.coins);
+    } else if (state is ListCoinLoading) {
+      return const Center(child: CircularProgressIndicator());
+    } else {
+      return const Center(child: Text(AppStrings.textEmptyList));
+    }
   }
 
   Widget listCoin({required List<ItemCoin> list}) {
